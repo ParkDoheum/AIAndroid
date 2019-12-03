6146082cfbecee659c583f9ea050204c79b1a4c0
epata94 <hklee0724@gmail.com>
2019-12-03 12:47:09 +0900

    버튼 이벤트

--- app/src/main/java/com/hkit/ddobang/CalcActivity.java
+++ app/src/main/java/com/hkit/ddobang/CalcActivity.java
@@ -3,6 +3,7 @@ package com.hkit.ddobang;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 public class CalcActivity extends AppCompatActivity {
@@ -12,7 +13,11 @@ public class CalcActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calc);
-
         et_input = findViewById(R.id.et_input);
+        et_input.setText("Hello 안녕!!");
+    }
+
+    public void btnClk(View v) {
+        et_input.setText("!!!!!");
     }
 }
